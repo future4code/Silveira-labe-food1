8daa2ebee669eba11db1f69eb601fe81efcd8e9a
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-05-21 20:49:40 -0300

    estilizacao carrinho

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,106 +1,154 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { DivTitle, Line, DivEndereco, DivEnderecoPessoa, DivEntrega, DivNomeRest } from './styled'
-import { DivRestaurante, DivTempo, DivEndRest, DivFrete, DivSubtotal, DivSubValor } from './styled'
-import axios from 'axios'
-import { BASE_URL } from '../../constants/urls'
-import GlobalStateContext from '../../context/GlobalStateContext'
-import { placeOrder } from "../../services/orders"
-import { useParams, useNavigate } from 'react-router-dom'
-import CardCart from "../../components/CardCart"
-import useForm from '../../hooks/useForm'
+import React, { useState, useContext, useEffect } from "react";
+import {
+  DivTitle,
+  Line,
+  DivEndereco,
+  DivEnderecoPessoa,
+  DivEntrega,
+  DivNomeRest,
+  Form,
+  DivForm,
+} from "./styled";
+import {
+  DivRestaurante,
+  DivTempo,
+  DivEndRest,
+  DivFrete,
+  DivSubtotal,
+  DivSubValor,
+} from "./styled";
+import axios from "axios";
+import { BASE_URL } from "../../constants/urls";
+import GlobalStateContext from "../../context/GlobalStateContext";
+import { placeOrder } from "../../services/orders";
+import { useParams, useNavigate } from "react-router-dom";
+import CardCart from "../../components/CardCart";
+import useForm from "../../hooks/useForm";
+import { Button, Typography } from "@mui/material";
+import { ScreenContainer } from "../EditAdress/styled";
 
 export default function CartPage(props) {
-
-  const { states, setters, requests } = useContext(GlobalStateContext)
+  const { states, setters, requests } = useContext(GlobalStateContext);
   const { cart, restaurantDetail, address } = states;
   const { setCart, setRestaurantDetail } = setters;
   const { getAddress } = requests;
-  const [payment, setPayment] = useState('');
-  const { form, InputChange, clear, setForm } = useForm({ neighbourhood: "", number: "", city: "", apartament: "", state: "", street: "" })
+  const [payment, setPayment] = useState("");
+  const { form, InputChange, clear, setForm } = useForm({
+    neighbourhood: "",
+    number: "",
+    city: "",
+    apartament: "",
+    state: "",
+    street: "",
+  });
 
-  const frete = restaurantDetail.shipping
+  const frete = restaurantDetail.shipping;
 
-  const subtotal = cart.reduce((acumulador, cart) =>
-    acumulador + cart.item.price * cart.item.quantity, 0)
+  const subtotal = cart.reduce(
+    (acumulador, cart) => acumulador + cart.item.price * cart.item.quantity,
+    0
+  );
 
   const clearCart = () => {
-    setCart([])
-  }
+    setCart([]);
+  };
 
   const confirmOrder = (restaurantDetail, subtotal) => {
     let productsArray = [];
-    cart && cart.forEach((order) => {
-      productsArray.push({ id: order.item.id, quantity: order.item.quantity })
-    })
-    const orderFinal = ({
+    cart &&
+      cart.forEach((order) => {
+        productsArray.push({
+          id: order.item.id,
+          quantity: order.item.quantity,
+        });
+      });
+    const orderFinal = {
       products: productsArray,
-      paymentMethod: payment
-    })
-    placeOrder(restaurantDetail, orderFinal, subtotal, clearCart)
-  }
+      paymentMethod: payment,
+    };
+    placeOrder(restaurantDetail, orderFinal, subtotal, clearCart);
+  };
 
   const listaCarrinho = cart.map((cart) => {
-    return (
-      <CardCart
-        product={cart.item}
-      />
-    )
-  }
-  )
+    return <CardCart product={cart.item} />;
+  });
 
   useEffect(() => {
-    getAddress(setForm)
-  }, [])
+    getAddress(setForm);
+  }, []);
 
   console.log(address.number);
   const restaurant = cart[0];
-  const {street, number} = address;
+  const { street, number } = address;
 
   return (
     <div>
-      <DivTitle>
-        Meu Carrinho
-      </DivTitle>
-
-      <DivEntrega>
-        <DivEndereco>Endereço de Entrega</DivEndereco>
-        {address && <DivEnderecoPessoa><p>{street}, {number}</p></DivEnderecoPessoa>}
-
-      </DivEntrega>
-
+      <DivTitle>Meu Carrinho</DivTitle>
+      <Typography>
+        <DivEndereco>Endereço de Entrega  {address && (
+          <DivEnderecoPessoa>
+              {street}, {number}
+          </DivEnderecoPessoa>
+        )}</DivEndereco>
+      </Typography>
       <DivRestaurante>
-       {restaurant &&
+        {restaurant && (
           <>
-            <DivNomeRest><h3>{restaurant.restaurantDetail.name}</h3></DivNomeRest>
-            <DivEndRest><p>{restaurant.restaurantDetail.address}</p></DivEndRest>
-            <DivTempo><p>{restaurant.restaurantDetail.deliveryTime} min</p></DivTempo>
+            <DivNomeRest>
+              <h3>{restaurant.restaurantDetail.name}</h3>
+            </DivNomeRest>
+            <DivEndRest>
+              <p>{restaurant.restaurantDetail.address}</p>
+            </DivEndRest>
+            <DivTempo>
+              <p>{restaurant.restaurantDetail.deliveryTime} min</p>
+            </DivTempo>
           </>
-
-        } 
-
+        )}
       </DivRestaurante>
-
       {listaCarrinho}
-
-      {cart.length === 0 && <DivFrete> frete: R$ 0</DivFrete>}
-
+      {cart.length === 0 && <DivFrete> Frete: R$ 0,00 </DivFrete>}
       <DivSubtotal>
         SUBTOTAL
-        <DivSubValor> R$ {subtotal.toFixed(2)}</DivSubValor>
+        <DivSubValor> R$ {subtotal.toFixed(2)}
+        </DivSubValor>
       </DivSubtotal>
-
       Forma de Pagamento
       <Line />
-      <form action="/action_page.php" onChange={(e) => setPayment(e.target.value)}>
-        <input type="radio" id="din" name="pagamento" value="money" />
-        <label for="din">Dinheiro</label><br></br>
-        <input type="radio" id="css" name="pagamento" value="creditcard" />
-        <label for="cartao">Cartão de Crédito</label>
-      </form>
-
-
-      <button onClick={() => confirmOrder(restaurantDetail, subtotal)}> Confirmar</button>
-
+      <br></br>
+       <DivForm>
+         <form
+          action="/action_page.php"
+          onChange={(e) => setPayment(e.target.value)}
+               >
+          <input type="radio" id="din" name="pagamento" value="money" />
+          <label for="din">Dinheiro</label>
+          <br></br>
+          <input type="radio" id="css" name="pagamento" value="creditcard" />
+          <label for="cartao">Cartão de Crédito</label>
+          </form>
+       </DivForm>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      
+      <Button
+        onClick={() => confirmOrder(restaurantDetail, subtotal)}
+        variant="contained"
+        fullWidth
+        color={"primary"}
+        >
+        {" "}
+        Confirmar
+      </Button>
     </div>
-  )
+  );
 }
--- src/pages/CartPage/styled.js
+++ src/pages/CartPage/styled.js
@@ -4,6 +4,8 @@ export const DivTitle = styled.div`
   text-align: center;
   font-weight: bold;
   font-size: 10px;
+  margin-top: 20px;
+  margin-bottom: 10px;
 `
 
 export const Line = styled.div`
@@ -13,15 +15,21 @@ export const Line = styled.div`
 `
 
 export const DivEndereco = styled.div`
-    color:#D1D1D6; 
+    color: grey; 
+    background-color: #D1D1D6;
+    padding: 15px 15px;
+    
 `
 export const DivEnderecoPessoa = styled.div`
     font-weight: bold;
+    margin-left: 5 px;
+    color: black;
 `
 
 export const DivEntrega = styled.div`
     color: black; 
     height: 15vh;
+
 `
 export const DivRestaurante = styled.div`
 height: 15vh;
@@ -38,16 +46,25 @@ color:black;
 export const DivFrete = styled.div`
 font-weight: bold;
 text-align: right;
+margin-right: 10px;
 `
 
 export const DivSubtotal= styled.div`
  display: flex;
 flex-direction: row;
 justify-content: space-between;
+margin-right: 10px;
+margin-left: 10px;
 `
 
 export const DivSubValor = styled.div`
 color:#E86E5A;
+padding: 5px;
+font-weight: bold;
+
+`
+export const DivForm = styled.div`
+padding: 20px;
 `
 
 export const ButtonAdd = styled.button`
@@ -79,3 +96,7 @@ background-color: white;
 color: red;
 border: 2px solid #555555;
 `
+
+// export const Button = styled.button`
+
+// `
