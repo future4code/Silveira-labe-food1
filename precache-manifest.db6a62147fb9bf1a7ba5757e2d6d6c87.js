0cbdbe851885e4eacdec60d188ef202d582dd77e
Karen Kubo <karennckubo@gmail.com>
2022-06-27 12:55:43 -0300

    Updates

---
+++ precache-manifest.db6a62147fb9bf1a7ba5757e2d6d6c87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba46c0588ab1ea63800da1e93f32aa0d",
+    "url": "/rappi4a/index.html"
+  },
+  {
+    "revision": "5fe3f276a22e0eea3fc8",
+    "url": "/rappi4a/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "8353ff852879b8f90b1a",
+    "url": "/rappi4a/static/css/main.daf496fa.chunk.css"
+  },
+  {
+    "revision": "5fe3f276a22e0eea3fc8",
+    "url": "/rappi4a/static/js/2.8697f602.chunk.js"
+  },
+  {
+    "revision": "bd6938412d04945efb76e6f456265208",
+    "url": "/rappi4a/static/js/2.8697f602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8353ff852879b8f90b1a",
+    "url": "/rappi4a/static/js/main.fbee2b95.chunk.js"
+  },
+  {
+    "revision": "1452be222e99a40dd403",
+    "url": "/rappi4a/static/js/runtime-main.00159ea9.js"
+  },
+  {
+    "revision": "478f123e749504dfe808a2109a392bc2",
+    "url": "/rappi4a/static/media/Logo.478f123e.png"
+  }
+]);
